c7b2fdb80168e7fb22647d95dcdbe0e36861cb6a
Kris <carsten@jaksch.me>
2022-08-23 20:00:10 +0000

    POC: Dynamic import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	async function loadComponent(name: string) {
+		return await import(`$lib/components/${name}.svelte`);
+	}
+
+	const components = [{ name: 'Component1' }, { name: 'Component2' }];
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each components as component}
+	{#await loadComponent(component.name) then module}
+		<svelte:component this={module.default} />
+	{/await}
+{/each}
